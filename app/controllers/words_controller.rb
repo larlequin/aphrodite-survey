5455ced4d2bd798c8fccbd96f502e490d2437395
Sébastien BEAU <sebastien.beau@akretion.com>
2014-09-12 00:19:32 +0200

    clean up code, use cocoon correctly (I hope), make the answer required, still need to fix the error message

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -8,38 +8,25 @@ class WordsController < ApplicationController
 
   def answers
     @word = Word.find(params[:id])
-    @questions = []
-
-    session[:question_group_ids].each do | group_id |
-      @word.questions.each do | question |
-        if question.group_id == group_id
-          @questions.append(question)
-        end
-      end
-    end
   end
 
   def update
-    word_params.each do | id, value |
-        answer = Answer.new(
-            word_id: params[:id],
-            user_id: session[:user_id],
-            question_id: id,
-            value: value["answers"]["value"],
-        )
-        answer.save
-    end
-    if session[:word_ids].size == 0
-      user = User.find(session[:user_id])
-      user.stop = Time.now
-      user.save
-      reset_session
-      redirect_to '/end'
-    else
-      word_id = session[:word_ids].shift
-      session[:current_word_id] = word_id
-      redirect_to :controller => 'words', :action => 'answers', :id => word_id
-    end
+      @word = Word.find(params[:id])
+      if @word.update(word_params)
+        if session[:word_ids].size == 0
+          user = User.find(session[:user_id])
+          user.stop = Time.now
+          user.save
+          reset_session
+          redirect_to '/end'
+        else
+          word_id = session[:word_ids].shift
+          session[:current_word_id] = word_id
+          redirect_to :controller => 'words', :action => 'answers', :id => word_id
+        end
+      else
+        render action: 'answers', notice: 'Perfect!'
+      end
   end
 
   private
@@ -51,7 +38,7 @@ class WordsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     # TODO
     def word_params
-      params.require(:word) # TODO FIXME.permit("question_1", "question_2", "question_3")
+      params.require(:word).permit(:word, :answers_attributes=>[:user_id, :question_id, :value])
     end
 end
 
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,6 +1,8 @@
 class Word < ActiveRecord::Base
   has_and_belongs_to_many :questions
-
+  has_many :answers
+  accepts_nested_attributes_for :answers, :reject_if => :all_blank
+  
   def get_question(user_id)
     user = User.find(user_id)
     question_group_ids = user.student_group.question_group_ids
