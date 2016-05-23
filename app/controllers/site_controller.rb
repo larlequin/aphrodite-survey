a53d9a96dbcd6020da6017967e3e1adeb4808642
Sébastien BEAU <sebastien.beau@akretion.com>
2016-05-23 23:04:21 +0200

    [IMP] add additionnal step after register an user, add logout feature, redirect to the survey if the user have already started the questionnary, set the start time when the user start the survey and not at the user creation

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,35 @@
 class SiteController < ApplicationController
   def index
+    if session[:start]
+      redirect_to :controller => 'words',
+        :action => 'answers',
+        :id => session[:current_word_id]
+    end
   end
+
+  def login
+    #TODO
+  end
+
+  def logout
+    reset_session
+  end
+
+  def start
+    @user = User.find(session[:user_id])
+    @user.start = Time.now
+    @user.end_of_session = Time.now + 90.minutes
+    @user.save
+    session[:user_id] = @user.id
+    session[:user_name] = @user.name
+    group_id = @user.id.modulo(12) + 1
+    session[:word_ids] = Word.where(group_id: group_id).pluck(:id).shuffle
+    session[:total_word] = session[:word_ids].size
+    word_id = session[:word_ids].shift
+    session[:current_word_id] = word_id
+    session[:start] = @user.start
+    session[:end_of_session] = @user.end_of_session
+    redirect_to :controller => 'words', :action => 'answers', :id => word_id
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'site#index'
   get '/end', to: 'site#end'
+  get '/logout', to: 'site#logout'
+  get '/start', to: 'site#start'
 
   resources :words do
     get 'answers', on: :member
